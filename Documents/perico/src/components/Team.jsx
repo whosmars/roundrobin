387983df8be2c6d58ed4ac272214e203a8ec0b7e
whosmars <mariosalazar@ciencias.unam.mx>
2024-11-24 01:15:07 -0600

    no se que hice pero es la casi final, esta es la del fallo de actualizacion por dunne

---
+++ Documents/perico/src/components/Team.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Conoce a las caras detras de UNAMarket</h2>
+          <p>
+            Nuestro equipo de trabajo es pequeño, pero de gran etica y compromiso
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ Documents/perico/src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Nuestra Vision</h2>
+          <p>
+          Nuestro objetivo es mejorar la calidad de vida de los estudiantes a través de innovación, colaboración y prácticas responsables, creando un impacto positivo en la comunidad universitaria.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
